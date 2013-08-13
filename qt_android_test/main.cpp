93c8f45570fd1a63e4e90148c24170240265af28
liaods <dongsheng.liao@gmail.com>
2013-08-13 12:57:38 +0800

    add test code for uart/pwm/adc

--- qt_android_test/main.cpp
+++ qt_android_test/main.cpp
@@ -20,16 +20,47 @@
 #include <Arduino.h>
 
 static int led_pin = 18;
+static int button_pin = 8;
+static int adc_val[6];
+static int value;
 
 void setup(void)
 {
     pinMode(led_pin, OUTPUT);
+    pinMode(button_pin, INPUT_PULLUP);
+    Serial.begin(115200);
 }
 
 void loop(void)
 {
+    Serial.print("\n======================================================================\n");
+    Serial.print("pcDuino Demo for qt-android v0.1\n");
+    Serial.print("Serial test: Hello, pcDuino\n");
+    Serial.print("pinMode/digitalWrite test: blinking TX LED\n");
     digitalWrite(led_pin, HIGH);
+    Serial.print("digitalRead test: pin level of GPIO");
+    Serial.print(button_pin, DEC);
+    value = digitalRead(button_pin);
+    Serial.println(value? ":HIGH":":LOW");
+
+    Serial.print("analogRead test:");
+    analogWrite(5, 0);
+    analogWrite(6, 0);
+    for( int i = 0; i<6; ++i)
+    {
+        adc_val[i] = analogRead(i);
+
+        Serial.print(" A");
+        Serial.print(i, DEC);
+        Serial.print("(");
+        Serial.print(adc_val[i], DEC);
+        Serial.print(") ");
+    }
     delay(1000);
+    Serial.print("\nanalogWrite test: PWM5(520 Hz, 50% cycle) PWM6(781 Hz, 25% cycle)\n");
+    analogWrite(5, pwmfreq_set(5, 520)/2);
+    analogWrite(6, pwmfreq_set(6, 781)/4);
+    Serial.print("======================================================================\n");
     digitalWrite(led_pin, LOW);
     delay(1000);
 }
